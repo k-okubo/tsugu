d343930902aa96b1887365a4e86d33e6b3b1c809
Koichiro Okubo <koichiro.okubo@gmail.com>
2019-10-15 10:38:22 +0900

    refactor directory structure

---
+++ test/lib/core_client.c
@@ -0,0 +1,30 @@
+/*--------------------------------------- vi: set ft=c ts=2 sw=2 et: --*-c-*--*/
+/**
+ * @file core_client.c
+ *
+ ** --------------------------------------------------------------------------*/
+
+#include <tsugu/core/parser.h>
+#include <tsugu/core/resolver.h>
+#include <tsugu/core/scanner.h>
+#include <tsugu/core/verifier.h>
+
+void _start(void) {
+  tsg_scanner_t* scanner = tsg_scanner_create("", 0);
+  tsg_parser_t* parser = tsg_parser_create(scanner);
+  tsg_ast_t* ast = tsg_parser_parse(parser);
+
+  tsg_parser_destroy(parser);
+  tsg_scanner_destroy(scanner);
+
+  tsg_resolver_t* resolver = tsg_resolver_create();
+  tsg_resolver_resolve(resolver, ast);
+  tsg_resolver_destroy(resolver);
+
+  tsg_verifier_t* verifier = tsg_verifier_create();
+  tsg_tyenv_t* tyenv = NULL;
+  tsg_verifier_verify(verifier, ast, &tyenv);
+
+  tsg_tyenv_destroy(tyenv);
+  tsg_ast_destroy(ast);
+}
